700833f35c68cbe7d42cb00f9e756f6c5756a019
Samuel Kipnis <sam.kipnis@gmail.com>
2024-01-16 10:15:17 -0500

    make gooder

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Idea: Dice Roller
 
+const rolls = [];
+
 const DOM = {
 	form: document.getElementById('form'),
 	numSidesInput: document.getElementById('numSides'),
-	numRollsInput: document.getElementById('numRolls'),
 	guessNumInput: document.getElementById('guessNum'),
-	guessValueInput: document.getElementById('guessValue'),
 	results: document.getElementById('results'),
-	prev: document.getElementById('pre'),
+	prev: document.getElementById('prev'),
 };
 
 function roll(numSides) {
@@ -15,65 +15,45 @@ function roll(numSides) {
 	return randNum;
 }
 
-function rollMult(numSides, numRolls, guessNum, guessValue) {
-	const rolls = {};
-	prev.insertAdjacentHTML('afterbegin', results.innerHTML + '<br>');
-	results.innerHTML = '';
-
+function isValidRoll(numSides, guessNum) {
 	if (guessNum < 1 || guessNum > numSides) {
 		alert(`Your guess must be a whole number between 1 and ${numSides}`);
-		return;
+		return false;
 	}
 
-	if (
-		numSides > 0 &&
-		Number.isInteger(numSides) &&
-		numRolls > 0 &&
-		Number.isInteger(numRolls)
-	) {
-		for (let i = 0; i < numRolls; i++) {
-			const randNum = roll(numSides);
-			rolls[randNum] = (rolls[randNum] || 0) + 1;
-		}
+	if (numSides > 0 && Number.isInteger(numSides)) {
+		return true;
 	} else {
 		alert('You must enter only whole numbers.');
+		return false;
 	}
+}
 
-	Object.keys(rolls).forEach((key) => {
-		console.log('hi');
-		const listEl = document.createElement('li');
-		if (Number(key) === guessNum) {
-			const bold = document.createElement('b');
-			bold.textContent = `${key}: ${rolls[key]} time${
-				rolls[key] > 1 ? 's' : ''
-			}`;
-			listEl.append(bold);
-
-			if (rolls[key] === guessValue) {
-				listEl.innerHTML += '<br> Correct Guess ✅';
+function displayData(rolls, guessNum) {
+	DOM.results.innerHTML = '';
+	DOM.prev.innerHTML = '';
+	for (let i in rolls) {
+		if (i == 0) {
+			DOM.results.innerHTML += `<p>You rolled a ${rolls[i]}</p>`;
+			if (rolls[i] == guessNum) {
+				DOM.results.innerHTML += `<p class='green'>Correct Guess</p>`;
 			}
 		} else {
-			listEl.textContent = `${key}: ${rolls[key]} time${
-				rolls[key] > 1 ? 's' : ''
-			}`;
+			DOM.prev.innerHTML += `<li>You rolled a ${rolls[i]}</li>`;
+			if (rolls[i] == guessNum) {
+				DOM.prev.innerHTML += `<p class='green'>Correct Guess</p>`;
+			}
 		}
-
-		results.append(listEl);
-	});
+	}
 }
 
 DOM.form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	console.log(
-		Number(DOM.numSidesInput.value),
-		Number(DOM.numRollsInput.value),
-		Number(DOM.guessNumInput.value),
-		Number(DOM.guessValueInput.value)
-	);
-	rollMult(
-		Number(DOM.numSidesInput.value),
-		Number(DOM.numRollsInput.value),
-		Number(DOM.guessNumInput.value),
-		Number(DOM.guessValueInput.value)
-	);
+	if (isValidRoll(Number(DOM.numSidesInput.value), Number(DOM.guessNumInput.value))) {
+		const randNum = roll(Number(DOM.numSidesInput.value));
+		rolls.unshift(randNum);
+
+		console.log(rolls);
+		displayData(rolls, Number(DOM.guessNumInput.value));
+	}
 });
